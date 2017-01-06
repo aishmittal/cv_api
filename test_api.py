7610c21b61f3fb6859d4660fd23043cdfa19458f
Aishwarya Mittal <aishwaryamittal2011@gmail.com>
2017-01-06 20:50:35 +0530

    Made Repo

---
+++ test_api.py
@@ -0,0 +1,22 @@
+# import the necessary packages
+import requests
+import cv2
+
+# define the URL to our face detection API
+url = "http://localhost:8000/face_detection/detect/"
+
+# use our face detection API to find faces in images via image URL
+# load our image and now use the face detection API to find faces in
+# images by uploading an image directly
+image = cv2.imread("adrian.jpg")
+payload = {"image": open("adrian.jpg", "rb")}
+r = requests.post(url, files=payload).json()
+print "adrian.jpg: {}".format(r)
+
+# loop over the faces and draw them on the image
+for (startX, startY, endX, endY) in r["faces"]:
+	cv2.rectangle(image, (startX, startY), (endX, endY), (0, 255, 0), 2)
+
+# show the output image
+cv2.imshow("adrian.jpg", image)
+cv2.waitKey(0)
